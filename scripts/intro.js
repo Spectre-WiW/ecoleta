51bdc2944b061ea1c5c21d95e5acc24a98cbc74b
Wilian Lopes <wilianlopesdesouza@gmail.com>
2020-06-03 21:57:17 -0300

    Add files via upload

---
+++ scripts/intro.js
@@ -0,0 +1,76 @@
+// comentarios
+// document.write("Hello")
+
+// variaveis, tipos de dados
+
+// let myvar = "He"
+// const myconst="He"
+
+// document.write(myconst + myvar)
+
+// string 
+// const s1 = "Isso é uma string"
+// const s2 ='Isso é uma string'
+// const s3 =`Isso é uma string`
+
+// document.write(s1)
+
+// number
+// const n1 = 1
+// const n2 = 12
+
+// document.write(n1 + n2)
+
+//boolean - true ou false
+
+// const bTrue = true
+// const bFalse = false
+// document.write(bTrue)
+
+//objeto possuem propriedades funcionalidades
+
+// const pessoa = { 
+//   altura: "1,80m", 
+//   idade:24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande lógica de correr")
+
+//   }
+// }
+
+// // document.write(pessoa.correr())
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+//Funções -= Funcionalidades - Atalhos - Reuso de código
+// // registrar funcao
+// function sayMyName(name){
+//   document.write(name)
+// }
+//Executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+//condicionais
+
+// const notaFinal = 7
+
+// if(notaFinal < 5 ){
+// //    caminho 1
+//    document.write('Reprovado')
+//    }else{
+// //   caminho 2
+//      document.write("Aprovado")
+// }
+
+// loop repetições
+
+for(i = 0; i < 10; i++){
+    document.write(`<p>${i}</p>`)
+  }
